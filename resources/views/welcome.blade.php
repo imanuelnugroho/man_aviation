9169c67aa5161b85cf416f66ec724e2594d2ea2c
Imanuel Titar Nugroho <imanuelnugroho@gmail.com>
2020-08-02 13:10:12 +0800

    Passenger options

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,61 +22,6 @@
             <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        
-        <!-- Styles -->
-        <!-- <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style> -->
-        
     </head>
     <body>
         <div id="booking" class="section">
@@ -155,11 +100,12 @@
                                                     <option>3</option>
                                                     <option>4</option>
                                                     <option>5</option>
+                                                    <option>6</option>
+                                                    <option>7</option>
+                                                    <option>8</option>
+                                                    <option>9</option>
                                                     <option>10</option>
-                                                    <option>50</option>
-                                                    <option>100</option>
-                                                    <option>200</option>
-                                                    <option>> 200</option>
+                                                    <option>> 10</option>
                                                 </select>
                                                 <span class="select-arrow"></span>
                                             </div>
